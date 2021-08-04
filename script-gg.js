f5c5c3d4dffd987c953ba78466c2d7d5a4f2a045
Aman Pardeshi <amanpardeshi@yahoo.co.in>
2021-08-04 14:40:40 +0530

    adding starting project

---
+++ script-gg.js
@@ -0,0 +1,26 @@
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+let randomColorGenerator = document.querySelector(".randomButton");
+// console.log(body);
+
+function setGraident() {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  css.textContent = body.style.background + ";";
+  console.log(color1.value, color2.value);
+}
+
+function randomColor() {
+  let randomColor1 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  let randomColor2 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  body.style.background =
+    "linear-gradient(to right, " + randomColor1 + ", " + randomColor2 + ")";
+  css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGraident);
+color2.addEventListener("input", setGraident);
+randomColorGenerator.addEventListener("click", randomColor);
+// *******************
