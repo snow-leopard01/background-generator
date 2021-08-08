241da5cecaa1ce2d3e8e27fcfd0966336a02be9d
Aman Pardeshi <amanpardeshi@yahoo.co.in>
2021-08-08 15:07:38 +0530

    adding json file

--- script-gg.js
+++ script-gg.js
@@ -1,3 +1,6 @@
+var _ = require("lodash");
+console.log(_);
+
 let css = document.querySelector("h3");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
@@ -23,4 +26,3 @@ function randomColor() {
 color1.addEventListener("input", setGraident);
 color2.addEventListener("input", setGraident);
 randomColorGenerator.addEventListener("click", randomColor);
-// *******************
